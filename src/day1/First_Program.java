823fe5be93665415603b01b0e1f41c853e8c5b5a
aniket1 <aniket6923@gmail.com>
2025-07-02 19:15:44 +0530

    this is a 1st push

---
+++ src/day1/First_Program.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class First_Program 
+{
+    public static void main(String args []) {
+    	System.out.println("Welcome to Java Learing");
+    	System.out.println("Hello World");
+    }
+}
---
+++ src/day1/SecoundProgram.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class SecoundProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		System.out.println("1+5");
+		System.out.println(1+5);
+		System.out.println("Welcome"+123);
+
+	}
+
+}

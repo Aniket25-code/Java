942f52a3654b65618efc53e1c57a35351598dfaa
aniket1 <aniket6923@gmail.com>
2025-07-10 20:22:06 +0530

    Day 2

---
+++ src/day2/DataTypeDemo.java
@@ -0,0 +1,49 @@
+package day2;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		// Numeric data types
+		
+		int a=100 ,b=200;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+		
+		
+		byte by=120;
+		System.out.println(by);
+		
+		Short sh=250;
+		System.out.println(sh);
+		
+		long l=12345679009090090L;// after 10 digit L(literal) char have to put at the end 
+		System.out.println(l);
+		
+		//Decimal number -float double
+		
+		float price=150.5f; // literanl is needed at end 
+		System.out.println(price);
+		
+		double item_price=145.23342;
+		System.out.println(item_price);
+		
+		//charatcter
+		
+	     char grad='B';
+	     System.out.println("the grade of the student is: "+ grad);
+	     
+	     //boolean - allowed only true/ false
+	     boolean bool=true;
+	     System.out.println(bool);
+	     
+	     //String
+	     String name="john";
+	     System.out.println(name);
+		
+			
+		
+
+	}
+
+}
---
+++ src/day2/VariablesDemo.java
@@ -0,0 +1,38 @@
+package day2;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int a;  //declaration
+		a=100;  //assignment
+		System.out.println(a); //100
+		a=200;
+		System.out.println(a);
+		*/
+		
+		/*int a=100; //declaration+assignment
+		System.out.println(a);
+		a=200;
+		System.out.println(a);*/
+		
+		// eg3 if all the varibale are belong to the same data type
+		/*int a , b ,c;
+		a=100;
+		b=200;
+		c=300;
+		System.out.println(c);*/
+		
+		//eg 5 -in single statement we defines mulple variables
+		
+		int a=100,b=200,c=300;
+		System.out.println("The value of A is: " +a);
+		System.out.println("The value of b is: " +b);
+		System.out.println("The velue of c is: " +c);
+		
+		System.out.println(a+ " "+b+" "+c); 
+	
+		}
+
+}

fe7f883a74625538feaa2eaadec61ea75fb3aff3
aniket1 <aniket6923@gmail.com>
2025-07-15 18:59:48 +0530

    change in logic

--- src/javaprograms/SwappingTwoNumbers.java
+++ src/javaprograms/SwappingTwoNumbers.java
@@ -5,12 +5,17 @@ public class SwappingTwoNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=10, b=20;
+		int a=90, b=60;
 		System.out.println("Before swapping values are.. "+a+"  "+b);
 		
-		a=a+b;//30
+		/*a=a+b;//30
 		b=a-b;//10
-		a=a-b;//20
+		a=a-b;*///20
+		
+		a=a+b; //90+60=150
+		b=a-b; //150-60=90
+		a=a-b; //150-90=60
+		
 		
 		System.out.println("after swapping values are ..."+a+"  " +b);
 		

2146028f20b6173547701ad18105cb4103961833
aniket1 <aniket6923@gmail.com>
2025-07-15 23:45:32 +0530

    new program add

---
+++ src/day4/IfCondition.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		int person_age=22;
+		if(person_age>=18)
+		{
+			System.out.println("Eligible for the vote");
+		}
+		
+
+	}
+
+}
---
+++ src/javaprograms/LasrgestOf3Numbers.java
@@ -0,0 +1,23 @@
+package javaprograms;
+
+public class LasrgestOf3Numbers {
+
+	public static void main(String[] args) {
+		int a=11,b=6,c=10;
+		if(a>b && a>c)
+		{
+			System.out.println("a is the largest number");
+		}
+		else if(b>a && b>c) 
+		{
+			System.out.println("b is the largest number");
+		}
+		else
+		{
+			System.out.println("c is the largest number");
+		}
+		
+	
+	}
+
+}

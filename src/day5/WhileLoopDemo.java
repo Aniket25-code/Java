3216e5bba6b32fed544d73fd39e0b35acf55cd80
aniket1 <aniket6923@gmail.com>
2025-07-25 22:27:48 +0530

    Day 5 push

---
+++ src/day5/WhileLoopDemo.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// example 1......10
+		
+		int i=1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+
+	}
+
+}
